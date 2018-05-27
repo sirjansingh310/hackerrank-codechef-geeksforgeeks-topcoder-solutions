4935dd976e939c869d12c2b5ecf5158e8a0dba4a
sirjansingh310 <sirjansingh310@gmail.com>
2018-05-27 15:58:33 +0530

    Create Building a list- Binary Subset solution.py

---
+++ Building a list- Binary Subset solution.py
@@ -0,0 +1,19 @@
+t = int(input())
+for tests in range(t):
+    n = input()
+    s = input()
+    o = []
+    length = "0"+str(len(s))+"b"
+    for i in range(1<<len(s)):
+        op = ""
+        binary = format(i,length)
+        for j in range(len(binary)):
+            if binary[j]=='1':
+                op+=s[j]
+        if len(op)>0:
+            o.append(op)
+    o.sort()
+    for i in o:
+        print (i)
+        
+        
