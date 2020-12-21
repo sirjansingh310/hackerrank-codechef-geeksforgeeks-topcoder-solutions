b3cce59cc4d13fbfb296103a0d8c13196142da0d
Sirjan Singh <sirjansingh310@gmail.com>
2020-12-21 15:41:56 +0530

    Update Leetcode - Smallest Subtree with all the Deepest Nodes.java

--- Leetcode - Smallest Subtree with all the Deepest Nodes.java
+++ Leetcode - Smallest Subtree with all the Deepest Nodes.java
@@ -72,3 +72,29 @@ class Solution {
         return leafNodes.get(0);
     }
 }
+
+
+// Approach 2: DFS, recursion. For a node n, if n.left can go to leaves and n.right goes to leaves (i.e deepestlevel), then node n is the common ancestor
+// class Solution {
+    
+//     int deepestLevel = 0;
+//     TreeNode res = null;
+    
+//     public TreeNode subtreeWithAllDeepest(TreeNode root) {
+//         dfs(root, 0);
+//         return res;
+//     }
+    
+//     private int dfs(TreeNode root, int level) {
+//         if (root == null) return level;
+        
+//         int leftLevel = dfs(root.left, level + 1);
+//         int rightLevel = dfs(root.right, level + 1);
+        
+//         int curLevel = Math.max(leftLevel, rightLevel);
+//         deepestLevel = Math.max(deepestLevel, curLevel);
+//         if (leftLevel == deepestLevel && rightLevel == deepestLevel)
+//             res = root;
+//         return curLevel;
+//     }
+// }
