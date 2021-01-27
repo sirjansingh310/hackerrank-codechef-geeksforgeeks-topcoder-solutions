b76bdc5c84de9cb252acad0516ae9b46015338eb
Sirjan Singh <sirjansingh310@gmail.com>
2021-01-28 01:53:34 +0530

    Create Leetcode - Concatenation of Consecutive Binary Numbers.java

---
+++ Leetcode - Concatenation of Consecutive Binary Numbers.java
@@ -0,0 +1,23 @@
+//https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/582/week-4-january-22nd-january-28th/3618/
+// amazing solution
+
+class Solution {
+    private boolean isPowerOfTwo(int n) {
+        return (n & (n - 1)) == 0;
+    }
+    public int concatenatedBinary(int n) {
+        long current = 1L;
+        long length = 1L;
+        final long mod = 1000000007L;
+        
+        for (int i = 2; i <= n; i++) {
+            if (isPowerOfTwo(i)) { // power of two detected, length of binary increases
+                length++;
+            }
+            current = (current << length) % mod;// prev binary = 11100, now make it 1110000000 
+            current = (current + i) % mod;// Binary OR to fill in new zero Bits on right with new number, i.e concatinaton 
+        }
+        
+        return (int)(current % mod);
+    }
+}
