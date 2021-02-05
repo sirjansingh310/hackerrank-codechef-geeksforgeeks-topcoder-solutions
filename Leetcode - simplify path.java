46d59ef14d667a68c7a3a396702023cc8b1980ad
Sirjan Singh <sirjansingh310@gmail.com>
2021-02-05 14:38:10 +0530

    Create Leetcode - simplify path.java

---
+++ Leetcode - simplify path.java
@@ -0,0 +1,42 @@
+// https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/584/week-1-february-1st-february-7th/3629/
+class Solution {
+    public String simplifyPath(String path) {
+        
+        while (path.contains("//")) {
+            path = path.replace("//", "/");
+        }
+        
+        if (path.length() == 1) {
+            return "/";
+        }
+        
+        if (path.charAt(path.length() - 1) == '/') {
+            path = path.substring(0, path.length() - 1);
+        }
+        
+        String[] instructions = path.substring(1, path.length()).split("/");
+        
+        Stack<String> stack = new Stack<>();
+        
+        for (String s : instructions) {
+            if (s.equals(".")) {
+                continue;
+            } else if (s.equals("..") && stack.size() > 0) {
+                stack.pop();
+            } else if (!s.equals("..")){
+                stack.push(s);
+            }
+        }
+        System.out.println(stack);
+        List<String> results = new ArrayList<>(stack);
+        String simplePath = "/";
+        for (int i = 0; i < results.size(); i++) {
+            simplePath += results.get(i);
+            if (i != results.size() - 1) {
+                simplePath += "/";
+            }
+        }
+        
+        return simplePath;
+    }
+}
