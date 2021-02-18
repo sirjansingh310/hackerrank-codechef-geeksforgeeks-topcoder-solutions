ac37b48ef03063a0f79d0d2d73c0c0252d683ab6
Sirjan Singh <sirjansingh310@gmail.com>
2021-02-19 02:11:03 +0530

    Update Leetcode - Letter case permutation.java

--- Leetcode - Letter case permutation.java
+++ Leetcode - Letter case permutation.java
@@ -58,3 +58,33 @@ class Solution {
         return new ArrayList<String>(all);
     }
 }
+
+
+// a better solution using index position and character array. Modify the array, deep recursion, modify the array for lowecase(backtrack step if you will), deep recursion.
+// at deepest point, when at last char, append in list the string!
+class Solution {
+    public List<String> letterCasePermutation(String S) {
+        List<String> list = new LinkedList<>();
+        permHelper( S.toCharArray(), 0, list);
+        return list;        
+    }
+    
+    private void permHelper( char[] strArr, int pos, List<String> list ){
+        if( pos == strArr.length ){
+            list.add(new String(strArr));
+            return;
+        }
+        
+        if( strArr[pos] >='0' && strArr[pos] <= '9' ){
+            permHelper( strArr, pos + 1, list);
+            return;
+        }
+                
+        strArr[pos] = Character.toUpperCase(strArr[pos]);
+        permHelper( strArr, pos + 1, list);
+
+        strArr[pos] = Character.toLowerCase(strArr[pos]);
+        permHelper( strArr, pos + 1, list);
+        
+    }
+}
