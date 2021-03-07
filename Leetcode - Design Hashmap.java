e07f4f53c1c5c37e1ed129c2b8922e91c0a91fbb
Sirjan Singh <sirjansingh310@gmail.com>
2021-03-08 01:00:18 +0530

    Update Leetcode - Design Hashmap.java

--- Leetcode - Design Hashmap.java
+++ Leetcode - Design Hashmap.java
@@ -1,4 +1,5 @@
 // https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/588/week-1-march-1st-march-7th/3663/
+// No internal Hashing related DS used
 class MyHashMap {
     private String[][] buckets;
     /** Initialize your data structure here. */
