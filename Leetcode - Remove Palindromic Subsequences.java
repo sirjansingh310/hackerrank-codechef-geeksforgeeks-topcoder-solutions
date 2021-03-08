a98cbdb309d69b34c205eecbeb3716f283dcd6c3
Sirjan Singh <sirjansingh310@gmail.com>
2021-03-08 17:34:28 +0530

    Update Leetcode - Remove Palindromic Subsequences.java

--- Leetcode - Remove Palindromic Subsequences.java
+++ Leetcode - Remove Palindromic Subsequences.java
@@ -1,4 +1,29 @@
 //https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/589/week-2-march-8th-march-14th/3665/
+
+
+// Better solution, leveraging the fact that the string will contain only 2 characters
+class Solution {
+    private boolean isPalindrome(String s) {
+        for (int i = 0; i <= s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public int removePalindromeSub(String s) {
+        if (s.length() == 0) {
+            return 0;
+        }
+        if (isPalindrome(s)) {
+            return 1;
+        }
+        return 2; // the whole thing is not a palindrome, but string consists of 'a' & 'b'
+        // remove all 'b's as first palindrome subseq and then a. Max step required = 2
+    }
+}
+
+// original solution, will work for any string, didn't account the fact that the string will contain only 'a' and 'b'
 class Solution {
     private String removeProcessedPalindromeSubseq(char[] ch) {
         StringBuilder sb = new StringBuilder("");
