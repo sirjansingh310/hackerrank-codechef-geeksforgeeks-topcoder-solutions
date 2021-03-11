f81cc6d3899ee8c7654ad48a205564873ea979d8
Sirjan Singh <sirjansingh310@gmail.com>
2021-03-12 01:06:37 +0530

    Create Leetcode - Coin change.java

---
+++ Leetcode - Coin change.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/589/week-2-march-8th-march-14th/3668/
+// Minimum number of coins required to make a particular amount, from an array of coins(supply of each coin is unlimited)
+class Solution {
+    private Map<Integer, Integer> memo = new HashMap<>();
+    private int recur(int[] coins, int amount) {
+        if (amount < 0) {
+            return Integer.MAX_VALUE;
+        }
+        if (amount == 0) {
+            return 0;
+        }
+        if (memo.containsKey(amount)) {
+            return memo.get(amount);
+        }
+        int best = Integer.MAX_VALUE;
+        for (int coin : coins) {
+            if (amount - coin < 0) {
+                break;
+            }
+            int newCase = recur(coins, amount - coin);
+            if (newCase < Integer.MAX_VALUE) {
+                best = Math.min(best, newCase + 1);
+            }
+        }
+        memo.put(amount, best);
+        return best;
+    }
+    public int coinChange(int[] coins, int amount) {
+        Arrays.sort(coins);
+        int result = recur(coins, amount);
+        return result == Integer.MAX_VALUE ? - 1 : result;
+    }
+}
