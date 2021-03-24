12fd5b2b9af19c1ef7cf55b504c48f507b44d4af
Sirjan Singh <sirjansingh310@gmail.com>
2021-03-24 21:33:23 +0530

    Update Leetcode - 3sum with multiplicity.java

--- Leetcode - 3sum with multiplicity.java
+++ Leetcode - 3sum with multiplicity.java
@@ -1,3 +1,4 @@
+
 class Solution {
     private static final int MOD = 1000000007;
     private static final int NOT_POSSIBLE = Integer.MIN_VALUE;
@@ -31,5 +32,74 @@ class Solution {
         Arrays.sort(arr);
         // sorting and going in reverse so we subtract big numbers from sum fast and need not do recursion deep
         return isPossible(arr.length - 1, arr, 0, target);
+    }
+}
+
+
+
+// Better solution using P and C
+class Solution {
+    private static final int MOD = 1000000007;
+    public int threeSumMulti(int[] arr, int target) {
+        int[] freq = new int[101];
+        for (int i : arr) {
+            freq[i]++;
+        }
+        
+        // lets say we have 3 numbers x, y, z which make sum target
+        // if x != y != z, freq(x) * freq(y) * freq(z) is total cases possibile with x, y, z
+        
+        // if x == y, y != z, then nc2 of (freq(x)) * freq(z). Simple P and C ( example we have 50 red balls, we can pick 2 balls in 50c2 ways)
+        // if x == y == z. nc3 of (freq(x))
+        
+        long result = 0;
+        for (int x = 0; x <= 100; x++) {
+            for (int y = x + 1; y <= 100; y++) {
+                int z = target - x - y;
+                if (y < z && z <= 100) {
+                    result += (freq[x] * freq[y] * freq[z]);
+                    result %= MOD;
+                }
+            }
+        }
+        
+        // x == y, y != z
+        
+        for (int x = 0; x <=100; x++) {
+            int z = target - (2 * x);
+            if (x < z && z <= 100) {
+                result += (nc2(freq[x]) * freq[z]);
+                result %= MOD;
+            }
+        }
+        
+        // x != y, y == z
+        
+        for (int x = 0; x <= 100; x++) {
+            if (target % 2 == x % 2) {
+                int z = (target - x) / 2;// target - x == 2 * y as y == z
+                if (x < z && z <= 100) {
+                    result += (freq[x] * nc2(freq[z]));
+                }
+            }
+        }
+        // x == y == z
+        if (target % 3 == 0) {
+            int x = target / 3;
+            if (x >= 0 && x <= 100) {
+                result += nc3(freq[x]);
+                result %= MOD;
+            }
+        }
+        
+        return (int)result;
+    }
+    
+    private long nc2(long n) {
+        return n * (n - 1) / 2;
+    }
+    
+    private long nc3(long n) {
+        return n * (n - 1) * (n - 2) / 6;
     }
 }
