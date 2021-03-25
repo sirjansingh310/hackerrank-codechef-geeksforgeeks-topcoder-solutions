5a4590192cdc3e0b900548eb35afb89ec340f830
Sirjan Singh <sirjansingh310@gmail.com>
2021-03-25 20:28:35 +0530

    Update Leetcode Advantage Shuffle.java

--- Leetcode Advantage Shuffle.java
+++ Leetcode Advantage Shuffle.java
@@ -36,3 +36,43 @@ class Solution {
         return result;
     }
 }
+
+// GREEDY SOLUTION
+class Solution {
+    public int[] advantageCount(int[] A, int[] B) {
+        Map<Integer, Queue<Integer>> assigned = new HashMap<>(); // as numbers in list are not unique, we use queue as value of key. We are going to sort B array so need to maintain mapping as we need to 
+        // return answer which corresponds to original input B
+        for (int i : B) {
+            assigned.put(i, new LinkedList<>());
+        }
+        Queue<Integer> remaining = new LinkedList<>();
+        
+        int[] bCopy = B.clone();
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        for (int i = 0, j = 0; i < A.length; i++) {
+            if (A[i] > B[j]) {// as sorted, A[i] and B[j] are small. Use it, otherwise throw it out to put at last, i.e the ones who could not get beat. We want to maximize positions where A[i] > B[i]
+                assigned.get(B[j]).add(A[i]);
+                j++;
+            } else {
+                remaining.add(A[i]);
+            }
+        }
+        
+        
+        int[] answer = new int[B.length];
+        
+        
+        for (int i = 0; i < bCopy.length; i++) {
+            if (assigned.get(bCopy[i]).size() > 0) {
+                answer[i] = assigned.get(bCopy[i]).poll();
+            } else {
+                answer[i] = remaining.poll();
+            }
+        }
+        
+        return answer;
+    }
+}
