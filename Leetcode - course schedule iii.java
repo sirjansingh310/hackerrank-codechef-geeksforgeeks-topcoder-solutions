9da7af3ee5008175595552260ea1b1dfc60959fe
Sirjan Singh <sirjansingh310@gmail.com>
2021-05-03 00:26:05 +0530

    Update and rename Leetcode - couse schedule iii.java to Leetcode - course schedule iii.java

--- Leetcode - course schedule iii.java
+++ Leetcode - course schedule iii.java
@@ -1,8 +1,10 @@
 //https://leetcode.com/explore/challenge/card/may-leetcoding-challenge-2021/598/week-1-may-1st-may-7th/3729/
 class Solution {
     public int scheduleCourse(int[][] courses) {
-        Arrays.sort(courses, (c1, c2) -> c1[1] - c2[1]);
-        PriorityQueue<Integer> takenCoursesDurationPq = new PriorityQueue<>((c1, c2) -> c2 - c1);
+        Arrays.sort(courses, (c1, c2) -> c1[1] - c2[1]); // sort courses by deadline, so we can "consider" as many courses as possible.
+        PriorityQueue<Integer> takenCoursesDurationPq = new PriorityQueue<>((c1, c2) -> c2 - c1);// for the "considered" course, if it is not possible to take it,
+        // i.e totalTime + course time > course deadline, we can remove already added course with maximum course time. 
+        // Think about doing this, no loss in count, we remove one to add one. But our total time spend reduces, so it will be better in long run(i.e for upcoming courses)
         long totalTime = 0L;
         for (int i = 0; i < courses.length; i++) {
             if (totalTime + courses[i][0] <= courses[i][1]) {
