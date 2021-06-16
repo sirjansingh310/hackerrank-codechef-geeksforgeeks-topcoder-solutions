0e6ff64640547da46105466df88030b16c3684db
Sirjan Singh <sirjansingh310@gmail.com>
2021-06-16 17:29:47 +0530

    Update Leetcode - Generate Parentheses.java

--- Leetcode - Generate Parentheses.java
+++ Leetcode - Generate Parentheses.java
@@ -26,3 +26,39 @@ class Solution {
         return new ArrayList<>(set);
     }
 }
+
+
+// Faster solution
+// Instead of keeping track of opening - closing brackets (upDownCount), we can simply track number of opening, closing.
+// This will keep it  more strict to call recursion. More validations ahead of time checked before calling recur again.
+
+
+class Solution {
+    private Set<String> set = new HashSet<>();
+
+    private void recur(int index, int length, String s, int opening, int closing, int allowed) {
+        if (index > length) {
+            return;
+        }
+
+        if (index == length && opening == closing) {
+            set.add(s);
+        }
+
+        if (opening < allowed) {
+            recur(index + 1, length, s + "(", opening + 1, closing, allowed);
+        }
+        // to avoid )()(
+        if (closing < opening) {
+             recur(index + 1, length, s + ")", opening, closing + 1, allowed);
+        }
+
+    }
+
+
+    public List<String> generateParenthesis(int n) {
+        // create vaid parenthesis with string length = n * 2
+        recur(0, n * 2, "", 0, 0, n);
+        return new ArrayList<>(set);
+    }
+}
