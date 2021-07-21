069b8b0319933cf88ab8e59ffced89760e1fa8e3
Sirjan Singh <sirjansingh310@gmail.com>
2021-07-21 15:23:51 +0530

    Update Leetcode - Push Dominoes.java

--- Leetcode - Push Dominoes.java
+++ Leetcode - Push Dominoes.java
@@ -1,9 +1,9 @@
 // https://leetcode.com/explore/challenge/card/july-leetcoding-challenge-2021/610/week-3-july-15th-july-21st/3821/
 /*
 we divide dots (straight dominoes) into groups, which have boundaries L R or nothing(index 0 or index n - 1 is a dot)
-for each of these local groups, we see the context obj, which has all the info of the group and determine the final state
+for each of these local groups, we see the group obj, which has all the info of the group and determine the final state
 */
-class DominoeContext { // a local group of dots
+class DominoeGroup { // a local group of dots
     int count, startIndex, endIndex;
     boolean surroundedByFallingLeft, surroundedByFallingRight; // true if right dominoe on the left side and left dominoe on the right side
     
@@ -20,60 +20,60 @@ class DominoeContext { // a local group of dots
 
 class Solution {
     public String pushDominoes(String dominoes) {
-        List<DominoeContext> straightDominoesContexts = new ArrayList<>();
+        List<DominoeGroup> straightDominoesGroups = new ArrayList<>();
         
-        DominoeContext currentContext = null;
+        DominoeGroup currentGroup = null;
         for (int i = 0; i < dominoes.length(); i++) {
             char dominoe = dominoes.charAt(i);
     
-            if (currentContext != null && dominoe != '.') {
-                currentContext.endIndex = i - 1;
-                straightDominoesContexts.add(currentContext);
+            if (currentGroup != null && dominoe != '.') {
+                currentGroup.endIndex = i - 1;
+                straightDominoesGroups.add(currentGroup);
                 
                 if (dominoe == 'L') {
-                    currentContext.surroundedByFallingLeft = true;
+                    currentGroup.surroundedByFallingLeft = true;
                 }
-                currentContext = null;
+                currentGroup = null;
             }
             
-            if (currentContext == null && dominoe != '.' && i + 1 < dominoes.length() && dominoes.charAt(i + 1) == '.') {
-                currentContext = new DominoeContext();
-                currentContext.startIndex = i + 1;
+            if (currentGroup == null && dominoe != '.' && i + 1 < dominoes.length() && dominoes.charAt(i + 1) == '.') {
+                currentGroup = new DominoeGroup();
+                currentGroup.startIndex = i + 1;
                 
                 if (dominoe == 'R') {
-                    currentContext.surroundedByFallingRight = true;
+                    currentGroup.surroundedByFallingRight = true;
                 }
             }
             
             if (dominoe == '.') {
-                if (currentContext == null) {
-                    currentContext = new DominoeContext();
-                    currentContext.startIndex = i;
+                if (currentGroup == null) {
+                    currentGroup = new DominoeGroup();
+                    currentGroup.startIndex = i;
                 }
-                currentContext.count++;
+                currentGroup.count++;
             }
             
         }
         
-        if (currentContext != null) {
-            currentContext.endIndex = dominoes.length() - 1;
-            straightDominoesContexts.add(currentContext);
+        if (currentGroup != null) {
+            currentGroup.endIndex = dominoes.length() - 1;
+            straightDominoesGroups.add(currentGroup);
         }
         
         char[] arr = dominoes.toCharArray();
-        convertDominoesToFinalState(arr, straightDominoesContexts);
+        convertDominoesToFinalState(arr, straightDominoesGroups);
         return new String(arr);
     }
     
-    private void convertDominoesToFinalState(final char[] arr, final List<DominoeContext> straightDominoesContexts) {
+    private void convertDominoesToFinalState(final char[] arr, final List<DominoeGroup> straightDominoesGroups) {
         
-        for (DominoeContext context : straightDominoesContexts) {
-            if (context.surroundedByFallingLeft && context.surroundedByFallingRight) {
-                for (int i = context.startIndex, filled = 0; i <= context.endIndex; i++, filled++) {
-                    if (context.count % 2 != 0 && filled == context.count / 2) {
+        for (DominoeGroup group : straightDominoesGroups) {
+            if (group.surroundedByFallingLeft && group.surroundedByFallingRight) {
+                for (int i = group.startIndex, filled = 0; i <= group.endIndex; i++, filled++) {
+                    if (group.count % 2 != 0 && filled == group.count / 2) {
                         continue;/// (RR...LL => RRR.LL odd number of count, middle one balances out)
                     }
-                    if (filled >= context.count / 2) {
+                    if (filled >= group.count / 2) {
                         arr[i] = 'L';
                     } else {
                         arr[i] = 'R';
@@ -81,13 +81,13 @@ class Solution {
                 }
             } else {
                 char toFill = '.'; // default
-                if (context.surroundedByFallingLeft) {
+                if (group.surroundedByFallingLeft) {
                     toFill = 'L';
-                } else if (context.surroundedByFallingRight) {
+                } else if (group.surroundedByFallingRight) {
                     toFill = 'R';
                 }
                 
-                for (int i = context.startIndex; i <= context.endIndex; i++) {
+                for (int i = group.startIndex; i <= group.endIndex; i++) {
                     arr[i] = toFill;
                 }
             }
