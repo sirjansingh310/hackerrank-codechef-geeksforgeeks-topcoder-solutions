72bf2de12d39ee6faca83e0272c53fb3786eb3ae
Sirjan Singh <sirjansingh310@gmail.com>
2021-08-05 16:25:07 +0530

    Update Leetcode -   Stone Game.java

--- Leetcode -   Stone Game.java
+++ Leetcode -   Stone Game.java
@@ -1,6 +1,17 @@
 // https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/613/week-1-august-1st-august-7th/3870/
+
+
+// Mathematical reasoning: o(1) alex wil always win the game !!!!!!!!!. There are even number of piles and total stones in all piles is odd, and alex is at advantage
+// by starting first.
+// At the end of the day, both alex and bob will have equal number of piles.
+// Lets say 2 piles.. [1, 12] Alex wins by taking 12 stoned pile
+// Let's say 4 piles .. [1,2,5,7] Alex is at advantage by starting first. Takes 7, and bob takes 5, alex takes 2 and wins the game.
+// This idea is true for all N when N is equal. Wow 
+
+// Programming approach, my solution
 // Both play optimally and the total stones are odd in number so there is never a tie.
 // We find the best possible alex score, if that is more than 50% of all score, that means alex will win. otherwise bob will win 
+// Time complexity: O(N^2)
 class Solution {
     private Map<String, Integer> map = new HashMap<>();
     
