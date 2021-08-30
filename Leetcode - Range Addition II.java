5e31fa61e31cd610888f0f936bd928b05f7c23ce
Sirjan Singh <sirjansingh310@gmail.com>
2021-08-30 14:39:16 +0530

    Update Leetcode - Range Addition II.java

--- Leetcode - Range Addition II.java
+++ Leetcode - Range Addition II.java
@@ -1,7 +1,7 @@
 // https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/617/week-5-august-29th-august-31st/3957/
 // For each operation, we are updating values from [0, 0] to [op[0], op[1]]. That means [0,0] will be updated for sure for every operation. 
 // We need to find number of cells in matrix which have the maximum value, i.e same value as mat[0][0] after all updates.
-// For each operation, the minimum among the x and y coordinates will be updated in all opeations. The product of these 2 minimums will be the number of cells which
+// For the given operations, the minimum among the x and y coordinates will be updated in all opeations. The product of these 2 minimums will be the number of cells which
 // were maximum updated.
 
 
