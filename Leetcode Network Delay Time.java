35e0fb86389d498c45d68259576a09652370b5ef
Sirjan Singh <sirjansingh310@gmail.com>
2022-05-26 02:49:15 +0530

    Create Leetcode Network Delay Time.java

---
+++ Leetcode Network Delay Time.java
@@ -0,0 +1,55 @@
+// https://leetcode.com/problems/network-delay-time/
+class Solution {
+    private class Pair implements Comparable<Pair> {
+        int node;
+        int time;
+        
+        public Pair(int node, int time) {
+            this.node = node;
+            this.time = time;
+        }
+        
+        public int compareTo(Pair other) {
+            return this.time - other.time;
+        }
+    }
+    
+    public int networkDelayTime(int[][] times, int n, int k) {
+        boolean[] visited = new boolean[n];
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+
+        int[] timesFromSource = new int[n];
+        Arrays.fill(timesFromSource, Integer.MAX_VALUE);
+        timesFromSource[k - 1] = 0;
+        pq.add(new Pair(k - 1, 0));
+        
+        while (!pq.isEmpty()) {
+            Pair min = pq.poll();
+            int node = min.node;
+            int parentTime = min.time;
+            visited[node] = true;
+            
+            for (int[] arr : times) {
+                int source = arr[0] - 1;
+                int dest = arr[1] - 1;
+                int timeCost = arr[2];
+                
+                if (source == node && !visited[dest] && timeCost + parentTime < timesFromSource[dest]) {
+                    timesFromSource[dest] = timeCost + parentTime;
+                    pq.add(new Pair(dest, timesFromSource[dest]));
+                }
+                
+            }
+        }
+
+        boolean allVisited = true;
+        for (boolean b : visited) {
+            if (!b) {
+                allVisited = false;
+                break;
+            }
+        }
+        int result = Arrays.stream(timesFromSource).boxed().max(Comparator.naturalOrder()).get();
+        return allVisited  ? result : -1;
+    }
+}
