b0530ec1db14fb9d33db2cbd3a8d566e443a6923
Sirjan Singh <sirjansingh310@gmail.com>
2022-05-26 13:36:11 +0530

    Update Leetcode Network Delay Time.java

--- Leetcode Network Delay Time.java
+++ Leetcode Network Delay Time.java
@@ -1,4 +1,5 @@
 // https://leetcode.com/problems/network-delay-time/
+
 class Solution {
     private class Pair implements Comparable<Pair> {
         int node;
@@ -17,6 +18,14 @@ class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
         boolean[] visited = new boolean[n];
         PriorityQueue<Pair> pq = new PriorityQueue<>();
+        Map<Integer, List<Pair>> adjMap = new HashMap<>();
+        
+        for (int[] arr : times) {
+            int source = arr[0] - 1;
+            int dest = arr[1] - 1;
+            int time = arr[2];
+            adjMap.computeIfAbsent(source, x -> new ArrayList<>()).add(new Pair(dest, time));
+        }
 
         int[] timesFromSource = new int[n];
         Arrays.fill(timesFromSource, Integer.MAX_VALUE);
@@ -29,16 +38,14 @@ class Solution {
             int parentTime = min.time;
             visited[node] = true;
             
-            for (int[] arr : times) {
-                int source = arr[0] - 1;
-                int dest = arr[1] - 1;
-                int timeCost = arr[2];
-                
-                if (source == node && !visited[dest] && timeCost + parentTime < timesFromSource[dest]) {
-                    timesFromSource[dest] = timeCost + parentTime;
-                    pq.add(new Pair(dest, timesFromSource[dest]));
+            if (adjMap.containsKey(node)) {
+                for (Pair p : adjMap.get(node)) {
+                    if (!visited[p.node] && p.time + parentTime < timesFromSource[p.node]) {
+                        timesFromSource[p.node] = p.time + parentTime;
+                        p.time = timesFromSource[p.node];
+                        pq.add(p);
+                    }
                 }
-                
             }
         }
 
