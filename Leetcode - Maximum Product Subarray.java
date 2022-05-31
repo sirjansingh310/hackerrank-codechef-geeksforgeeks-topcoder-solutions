ba12f2f1787a0b923d7b7b170dc9566bc482b3c7
Sirjan Singh <sirjansingh310@gmail.com>
2022-06-01 00:45:15 +0530

    Create Leetcode - Maximum Product Subarray.java

---
+++ Leetcode - Maximum Product Subarray.java
@@ -0,0 +1,62 @@
+// https://leetcode.com/problems/maximum-product-subarray/
+// Simple dp, using precomputed produts, same as prefix sum
+// divide into chunks of not containing zero, precompute their products.
+// if final product > 0, return it, otherwise it means we need to skip one negative number to get a positive product
+// iterate and choose max using our precomputed products array, product left of skipped number vs product on right of skipped number
+class Solution {
+    public int maxProduct(int[] nums) {
+        List<int[]> chunks = new ArrayList<>();
+        int start = 0;
+        int maxPro = Integer.MIN_VALUE;
+        
+        while (start < nums.length && nums[start] == 0) {
+            start++;
+        }
+        
+        if (start == nums.length) {
+            return 0;
+        }
+        
+        for (int i = start; i < nums.length; i++) {
+            if (i > 0 && nums[i] == 0) {
+                chunks.add(new int[]{start, i - 1});
+                start = i + 1;
+            }
+            maxPro = Math.max(maxPro, nums[i]);
+        }
+        
+        if (start < nums.length) {
+           chunks.add(new int[]{start, nums.length - 1}); 
+        }
+        
+        for (int[] ch : chunks) {
+            maxPro = Math.max(maxPro, getMaxPro(nums, ch[0], ch[1]));
+        }
+        return maxPro;
+    }
+    
+    private int getMaxPro(int[] nums, int start, int end) {
+        int[] products = new int[nums.length];
+        int currentProduct = 1;
+        int maxProduct = Integer.MIN_VALUE;
+        
+        for (int i = start; i <= end; i++) {
+            products[i] = currentProduct * nums[i];
+            currentProduct = products[i];
+        }
+        
+        if (products[end] > 0) {
+            return products[end];
+        } else { // get positve result by ignoring one negative number
+            for (int i = start; i <= end; i++) {
+                if (nums[i] < 0 && i > start) {
+                    maxProduct = Math.max(maxProduct, products[i - 1]);
+                }
+                if (nums[i] < 0 && i < end) {
+                    maxProduct = Math.max(maxProduct, products[end] / products[i]);
+                }
+            }
+            return maxProduct;
+        }
+    }
+}
