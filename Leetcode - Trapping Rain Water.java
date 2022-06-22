5c6ab94d733038d085fa2a323bdba362bec2b084
Sirjan Singh <sirjansingh310@gmail.com>
2022-06-23 01:57:13 +0530

    Update Leetcode - Trapping Rain Water.java

--- Leetcode - Trapping Rain Water.java
+++ Leetcode - Trapping Rain Water.java
@@ -27,25 +27,9 @@ class Solution {
         }
               
         for (int i = 0; i < height.length; i++) {
-            if (stack.isEmpty()) {
-                stack.push(i);
-            } else if (height[i] < height[stack.peek()]) { // continue inserting in monotinic stack. 
-                // this statement means stack peek element can still contribute as water can spill over to right. 
-                stack.push(i);
-            } else { // the new height barrier is more that stack top element.
-                // Meaning contribution of stack top element is over since water may not(possible to flow if height[i] is not the max from right) flow to right.
-                // this barrier may stop water to go spill over right. So we have to calculate its contribution.
-                // We already found which wall we need to consider above, so we use it. 
-                while (!stack.isEmpty() && height[i] >= height[stack.peek()]) {
-                    int elementIndex = stack.pop();
-                    if (wall[elementIndex] > height[elementIndex]) {
-                        water += (wall[elementIndex] - height[elementIndex]);
-                    }
-                    
-                }
-                stack.push(i);
+            if (wall[i] > height[i]) {
+                water += wall[i] - height[i];
             }
-            
         }
         
         return water;
