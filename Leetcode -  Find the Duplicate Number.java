8ef226e35d08e318d855496fb7ec48ea19db5f8a
Sirjan Singh <sirjansingh310@gmail.com>
2022-08-14 22:30:01 +0530

    Create Leetcode -  Find the Duplicate Number.java

---
+++ Leetcode -  Find the Duplicate Number.java
@@ -0,0 +1,28 @@
+// O(N) time and O(1) space without touching the original nums array
+// https://www.youtube.com/watch?v=wjYnzkAhcNk&ab_channel=NeetCode
+// Not a very intuitive solution. The question boils down to 
+// finding where loop starts in a linked list with loop
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = 0, fast = 0;
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]]; // jump by 2 positions
+            
+            if (slow == fast) {
+                break;
+            }
+        }
+        int slow2 = 0;
+        
+        while (true) {
+            slow = nums[slow];
+            slow2 = nums[slow2];
+            if (slow == slow2) {
+                break;
+            }
+        }
+        
+        return slow;
+    }
+}
