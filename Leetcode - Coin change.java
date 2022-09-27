fa3f9ac853ef20db7abbfe354b3af241c906c5be
Sirjan Singh <sirjansingh310@gmail.com>
2022-09-28 01:21:12 +0530

    Update Leetcode - Coin change.java

--- Leetcode - Coin change.java
+++ Leetcode - Coin change.java
@@ -31,3 +31,31 @@ class Solution {
         return result == Integer.MAX_VALUE ? - 1 : result;
     }
 }
+
+
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if (amount == 0 || coins.length == 0) {
+            return 0;
+        } 
+        Arrays.sort(coins);
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        for (int i = 0; i < coins.length; i++) {
+            if (coins[i] <= amount) {
+                dp[coins[i]] = 1;
+            }
+        }
+        
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i && dp[i - coins[j]] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
+                }
+            }
+        }
+        
+        return dp[amount] != Integer.MAX_VALUE ? dp[amount] : -1;
+    }
+}
