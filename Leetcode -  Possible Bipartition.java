3eb779a2889fefb6de57e9d27eb54039b9231e0c
Sirjan Singh <sirjansingh310@gmail.com>
2022-12-25 22:06:01 +0530

    Update Leetcode -  Possible Bipartition.java

--- Leetcode -  Possible Bipartition.java
+++ Leetcode -  Possible Bipartition.java
@@ -1,4 +1,7 @@
 // https://leetcode.com/problems/possible-bipartition/
+// Problem basically boils down to see if dislikes graph is a bipartite graph
+// https://en.wikipedia.org/wiki/Bipartite_graph
+// Union find solution
 class Solution {
     public boolean possibleBipartition(int n, int[][] dislikes) {
         Map<Integer, List<Integer>> adj = new HashMap<>();
@@ -51,5 +54,57 @@ class Solution {
             return root;
         }
         return x;
+    }
+}
+
+// More intuitive, bfs + graph coloring, color one set as red, other as blue, see if it holds true all the time, otherwise we have a conflict
+class Solution {
+    public boolean possibleBipartition(int n, int[][] dislikes) {
+        if (dislikes.length == 0) {
+            return true;
+        }
+        int[] color = new int[n + 1];// can we 0 or 1, -1 means not colored
+        Arrays.fill(color, -1); // will use this to also know if node is visited or not
+        
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        
+        for (int[] pair : dislikes) {
+            adj.get(pair[0]).add(pair[1]);
+            adj.get(pair[1]).add(pair[0]);
+        }
+        
+        for (int i = 1; i <= n; i++) { // since multiple connected components
+            if (color[i] == -1) {
+                if (!bfs(i, adj, color)) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+    
+    
+    private boolean bfs(int start, List<List<Integer>> adj, int[] color) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        color[start] = 1;
+        
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            for (int i : adj.get(current)) {
+                if (color[i] == -1) {
+                    color[i] = 1 - color[current]; // other color than parent
+                    queue.add(i);
+                } else if (color[i] == color[current]) {// conflict
+                    return false;
+                }
+            }
+        }
+        
+        return true;
     }
 }
